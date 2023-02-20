c7a959dfe1d7f2dd6b29aba4688e0a080b8f7c98
BarrusRho <60799948+BarrusRho@users.noreply.github.com>
2023-02-20 22:45:27 +0200

    - Restructures project files

---
+++ Assets/_Scripts/Core/ClearCounter.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+namespace KitchenSimulator.Core
+{
+    public class ClearCounter : MonoBehaviour
+    {
+        public void Interact()
+        {
+            Debug.Log("Interacting");
+        }
+    }
+    
+}
