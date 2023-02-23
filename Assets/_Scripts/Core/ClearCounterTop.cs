350cea03b036b8e7d5a6d0312357fb639e741bec
BarrusRho <60799948+BarrusRho@users.noreply.github.com>
2023-02-24 00:32:43 +0200

    - Sets the parent counterTop of ingredient

--- Assets/_Scripts/Core/ClearCounterTop.cs
+++ Assets/_Scripts/Core/ClearCounterTop.cs
@@ -7,17 +7,45 @@ namespace KitchenSimulator.Core
     public class ClearCounterTop : MonoBehaviour
     {
         [SerializeField] private Transform _counterTopSpawnPoint;
-        [FormerlySerializedAs("_kitchenObjectSO")] [SerializeField] private IngredientSO _ingredientSo;
-        
+        [SerializeField] private IngredientSO _ingredientSo;
+        private Ingredient _ingredient;
+
         public void Interact()
         {
-            Debug.Log("Interacting");
-            
-            var ingredientTransform = Instantiate(_ingredientSo.ingredientPrefab, _counterTopSpawnPoint);
-            ingredientTransform.localPosition = Vector3.zero;
-            
-            Debug.Log(ingredientTransform.GetComponent<Ingredient>().GetIngredientSo().ingredientName);
+            if (_ingredient == null)
+            {
+                var ingredientTransform = Instantiate(_ingredientSo.ingredientPrefab, _counterTopSpawnPoint);
+                ingredientTransform.GetComponent<Ingredient>().SetClearCounterTop(this);
+            }
+            else
+            {
+                Debug.Log(_ingredient.GetClearCounterTop());
+            }
+        }
+
+        public Transform GetIngredientFollowTransform()
+        {
+            return _counterTopSpawnPoint;
+        }
+
+        public void SetIngredient(Ingredient ingredient)
+        {
+            this._ingredient = ingredient;
+        }
+
+        public Ingredient GetIngredient()
+        {
+            return _ingredient;
+        }
+
+        public void ClearIngredient()
+        {
+            _ingredient = null;
+        }
+
+        public bool HasIngredient()
+        {
+            return _ingredient != null;
         }
     }
-    
 }
--- Assets/_Scripts/Core/Ingredient.cs
+++ Assets/_Scripts/Core/Ingredient.cs
@@ -6,10 +6,34 @@ namespace KitchenSimulator.Core
     public class Ingredient : MonoBehaviour
     {
         [SerializeField] private IngredientSO _ingredientSO;
+        private ClearCounterTop _clearCounterTop;
 
         public IngredientSO GetIngredientSo()
         {
             return _ingredientSO;
+        }
+
+        public void SetClearCounterTop(ClearCounterTop clearCounterTop)
+        {
+            if (this._clearCounterTop !=null)
+            {
+                this._clearCounterTop.ClearIngredient();
+            }
+            this._clearCounterTop = clearCounterTop;
+
+            if (clearCounterTop.HasIngredient())
+            {
+                Debug.Log($"CounterTop already has an ingredient!");
+            }
+            clearCounterTop.SetIngredient(this);
+            
+            transform.parent = clearCounterTop.GetIngredientFollowTransform();
+            transform.localPosition = Vector3.zero;
+        }
+
+        public ClearCounterTop GetClearCounterTop()
+        {
+            return _clearCounterTop;
         }
     }
 }
