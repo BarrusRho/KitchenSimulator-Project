961f4c34ef12354fb8da93f5ecb84c47cc9f3be1
BarrusRho <60799948+BarrusRho@users.noreply.github.com>
2023-03-06 00:42:18 +0200

    - Adds a TrashCounterTop object

--- Assets/_Scripts/CounterTops/TrashCounterTop.cs
+++ Assets/_Scripts/CounterTops/TrashCounterTop.cs
@@ -1,8 +1,16 @@
+using KitchenSimulator.Core;
 using UnityEngine;
 
 namespace KitchenSimulator.CounterTops
 {
-    public class TrashCounterTop : MonoBehaviour
+    public class TrashCounterTop : CounterTopBase
     {
+        public override void Interact(Player player)
+        {
+            if (player.HasIngredient())
+            {
+                player.GetIngredient().DestroySelf();
+            }
+        }
     }
 }
