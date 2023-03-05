8b5defd10578a5bf875a021a32be73dd6c15ee02
BarrusRho <60799948+BarrusRho@users.noreply.github.com>
2023-03-05 22:28:47 +0200

    - Adds functionality for the cutting progress bar to LookAt the main camera

---
+++ Assets/_Scripts/UI/LookAtCamera.cs
@@ -0,0 +1,38 @@
+using System;
+using UnityEngine;
+
+namespace KitchenSimulator.UI
+{
+    public class LookAtCamera : MonoBehaviour
+    {
+        private enum CameraMode
+        {
+            LookAt,
+            LookAtInverted,
+            CameraForward,
+            CameraForwardInverted
+        }
+
+        [SerializeField] private CameraMode _cameraMode;
+
+        private void LateUpdate()
+        {
+            switch (_cameraMode)
+            {
+                case CameraMode.LookAt:
+                    transform.LookAt(Camera.main.transform);
+                    break;
+                case CameraMode.LookAtInverted:
+                    var directionFromCamera = transform.position - Camera.main.transform.position;
+                    transform.LookAt(transform.position + directionFromCamera);
+                    break;
+                case CameraMode.CameraForward:
+                    transform.forward = Camera.main.transform.forward;
+                    break;
+                case CameraMode.CameraForwardInverted:
+                    transform.forward = -Camera.main.transform.forward;
+                    break;
+            }
+        }
+    }
+}
